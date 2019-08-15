eaa7c52665c30fed3f1e14a8db070b0d5615eef9
Kerem Kazan <kerem.kazan@gmail.com>
2019-08-15 15:02:20 -0700

    dev setup completed

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log("HI");
+import express from "express";
+import { ApolloServer, gql } from "apollo-server-express";
+
+const PORT = 4001;
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!"
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () => {
+  console.log(`Server ready at: http://localhost:${PORT}${server.graphqlPath}`);
+});
