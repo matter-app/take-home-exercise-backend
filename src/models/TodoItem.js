d4da409a43803ff36f6774ad85653b40403884d4
Kerem Kazan <kerem.kazan@gmail.com>
2019-08-15 16:41:18 -0700

    pluralized table name in TodoItem model

--- src/models/TodoItem.js
+++ src/models/TodoItem.js
@@ -1,14 +1,24 @@
-import Sequelize from "sequelize";
+import { Sequelize } from "sequelize";
+import { sequelize } from "../sequelize";
 
 export class TodoItem extends Sequelize.Model {}
 
-TodoItem.init({
-  parentItemId: Sequelize.INTEGER,
-  title: Sequelize.STRING,
-  isCompleted: Sequelize.BOOLEAN
-});
+TodoItem.init(
+  {
+    parentItemId: Sequelize.INTEGER,
+    title: Sequelize.STRING,
+    isCompleted: Sequelize.BOOLEAN
+  },
+  {
+    sequelize,
+    tableName: "todo_items"
+  }
+);
 
 TodoItem.belongsTo(TodoItem, {
   foreignKey: "parentItemId"
 });
-TodoItem.hasMany(TodoItem);
+
+TodoItem.hasMany(TodoItem, {
+  foreignKey: "parentItemId"
+});
---
+++ src/sequelize.js
@@ -0,0 +1,9 @@
+import { Sequelize } from "sequelize";
+import path from "path";
+
+const dbPath = path.join(__dirname, "../", "database.sqlite3");
+
+export const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: dbPath
+});
